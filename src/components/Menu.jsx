349c482624e5ce27e0306c1af477539875e88c4e
Angwyn <angwyn.r.d@gmail.com>
2022-12-06 22:51:45 +0800

    add menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import picmenu from './img/picmenu.svg'
 
 export const Menu = () => {
   return (
-    <div className='flex bg-[#0B1D37] pt-40 pb-40 mx-auto'>
-        <h1 className='text-white'>ketahui lebih lengkap mengenai komponen pc yang akan di beli</h1>
+    <div className='flex flex-col pt-10 pb-10 lg:flex-row lg:space-y-0 lg:items-center lg:content-center lg:pl-10 lg:pr-10 lg:mx-auto lg:space-x-5 xl:w-full xl:justify-center bg-[#0B1D37]'>
+        <div class="mx-auto">
+            <img src={picmenu} alt="" />
+        </div>
+        <div class="flex flex-col space-y-3 px-10">
+                <h1 class="text-white font-bold text-[26px] text-left sm:text-[26px] xl:text-[20px] lg:text-left lg:">
+                ketahui lebih lengkap mengenai komponen pc <br></br> yang akan di beli
+                </h1>
+            </div>
     </div>
   )
 }
