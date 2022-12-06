9f7159dbc0aedb32b94bbb2e6abab523a7621086
Angwyn <angwyn.r.d@gmail.com>
2022-12-06 21:59:55 +0800

    change navbar

---
+++ src/components/Menu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Menu = () => {
+  return (
+    <div className='flex bg-[#0B1D37] pt-40 pb-40 mx-auto'>
+        <h1 className='text-white'>ketahui lebih lengkap mengenai komponen pc yang akan di beli</h1>
+    </div>
+  )
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,9 +18,9 @@ export const Navbar = () => {
             <li className='p-4'>Home</li>
         </ul>
         <div onClick={handleNav} className='block md:hidden'>
-            {!nav ? <AiOutlineClose size={30}/> : <AiOutlineMenu size={30}/>}
+            {!nav ? <AiOutlineMenu size={30}/> : <AiOutlineClose size={30}/>}
         </div>
-        <div className={!nav ? 'fixed left-0 top-0 w-[40%] h-full border-r border-r-gray-900 bg-[#676767] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+        <div className={!nav ? 'fixed left-[-100%]' : 'fixed left-0 top-0 w-[40%] h-full border-r border-r-gray-900 bg-[#676767] ease-in-out duration-500' }>
             <h1 className='w-full text-3xl font-bold m-4'>PcRec</h1>
             <ul className=' uppercase p-4'>
                 <li className='p-4 border-b border-gray-600'>Home</li>
